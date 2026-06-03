632aa56fde50788c6ab1718e55221b09468f8bca
David Solet <dsolet@gmail.com>
2026-06-02 21:16:08 -0700

    Updated for April and May 2026.

--- pvhcReptProd.R
+++ pvhcReptProd.R
@@ -3,13 +3,13 @@
 # stores in appropriate Drive directory
 
 # pass report date values
-filterDte <- '2026-03-31' #last day of report period
+filterDte <- '2026-05-31' #last day of report period
 fy_start <- '2026-01-01'
-curr_mo_start <- '2026-03-01'
+curr_mo_start <- '2026-05-01'
 fy_to_date <- filterDte
 reptDte <- format(as.Date(fy_to_date), '%B %d, %Y')
-currM <- 'Mar' # month of report
-nextM <- 'Apr' # change to next month after report date
+currM <- 'May' # month of report
+nextM <- 'Jun' # change to next month after report date
 
 startDateSubTitle <- 'January 1, 2024'
 
